e8a4d4e833b41d692cef3388605d3c0f7e2f67d5
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-04-01 10:55:22 +0300

    Updates

---
+++ precache-manifest.30b19ce92a193e5f1deb190205a06ef5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268da3690719a4e521f3cc779d6ef922",
+    "url": "/TestProject/index.html"
+  },
+  {
+    "revision": "c80e00186bce1e2a8087",
+    "url": "/TestProject/static/css/main.400fba3d.chunk.css"
+  },
+  {
+    "revision": "b01586bdfa9d7018f05d",
+    "url": "/TestProject/static/js/2.d2b0a7ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestProject/static/js/2.d2b0a7ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80e00186bce1e2a8087",
+    "url": "/TestProject/static/js/main.bed3d606.chunk.js"
+  },
+  {
+    "revision": "80d186de49362f8f1104",
+    "url": "/TestProject/static/js/runtime-main.c73e59df.js"
+  },
+  {
+    "revision": "a560f017e07086326e64dee00c34b516",
+    "url": "/TestProject/static/media/image1-min.a560f017.jpeg"
+  },
+  {
+    "revision": "7fcb31b942fbc3cd425f47c26c6d0807",
+    "url": "/TestProject/static/media/image2-min.7fcb31b9.jpg"
+  },
+  {
+    "revision": "b045ba99ad47d3dfec0de32338ee8e2a",
+    "url": "/TestProject/static/media/image3-min.b045ba99.jpeg"
+  },
+  {
+    "revision": "9e622a3257ae2149fb207a6366c9fad9",
+    "url": "/TestProject/static/media/main-photo-min.9e622a32.jpg"
+  }
+]);
